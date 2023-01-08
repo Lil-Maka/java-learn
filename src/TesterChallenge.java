4ea7b6332f71600f1b9480ab9d313b17dcf5b569
Andrés <andresrosillo94@hotmail.com>
2023-01-08 02:35:24 +0100

    switch statement advanced, get days in month and leap year

--- src/TesterChallenge.java
+++ src/TesterChallenge.java
@@ -1,43 +1,32 @@
 public class TesterChallenge {
     public static void main(String[] args) {
-        printNumberInWord(3);
+        System.out.println(getDaysInMonth(1, 2020));
+        System.out.println(getDaysInMonth(2, 2020));
+        System.out.println(getDaysInMonth(2, 2018));
+        System.out.println(getDaysInMonth(-1, 2020));
+        System.out.println(getDaysInMonth(1, -2020));
     }
 
-    public static void printNumberInWord (int number) {
-        switch (number) {
-            case 0:
-                System.out.println("ZERO");
-                break;
-            case 1:
-                System.out.println("ONE");
-                break;
-            case 2:
-                System.out.println("TWO");
-                break;
-            case 3:
-                System.out.println("THREE");
-                break;
-            case 4:
-                System.out.println("FOUR");
-                break;
-            case 5:
-                System.out.println("FIVE");
-                break;
-            case 6:
-                System.out.println("SIX");
-                break;
-            case 7:
-                System.out.println("SEVEN");
-                break;
-            case 8:
-                System.out.println("EIGHT");
-                break;
-            case 9:
-                System.out.println("NINE");
-                break;
+    public static boolean isLeapYear(int year) {
+        if(year > 0 && year < 10000) {
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        } else {
+            return false;
+        }
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if(month < 1 || month > 12 || year < 1 || year > 9999) {
+            return -1;
+        }
+
+        boolean isLeapMonth = isLeapYear(year);
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
             default:
-                System.out.println("OTHER");
-                break;
+                if(month == 2) return isLeapMonth ? 29 : 28;
+                else return 30;
         }
     }
 }
