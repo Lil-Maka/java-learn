9a21b1ca4331130f0a205f92afe3f9779701d58d
Andrés <andresrosillo94@hotmail.com>
2023-01-11 11:48:23 +0100

    sum even numbers in digit algorithm

---
+++ src/SumEvenInDigit.java
@@ -0,0 +1,25 @@
+public class SumEvenInDigit {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
+
+    public static int getEvenDigitSum(int number) {
+        int totalEvenInDigitSum = 0;
+
+        if(number < 0) {
+            return -1;
+        }
+
+        while (number > 0) {
+            int currentValue = number%10;
+            number /= 10;
+
+            totalEvenInDigitSum += currentValue % 2 == 0 ? currentValue : 0;
+        }
+
+        return totalEvenInDigitSum;
+    }
+}
+}
